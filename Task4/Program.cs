4db676c602e31c5c935c5cafbc6030489fedc87f
Sultan Abdullaev <sultan230199@yandex.ru>
2024-01-07 17:12:53 +0300

    added lesson1/task4

---
+++ Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 1;
+int b = 4;
+int c = 8;
+int d = 10;
+int e = 15;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
